7c008f37e5d73d6a9f2ef2cf80eeb9398fcf92f2
Sepehr Saryazdi <sepehr.saryazdi@gmail.com>
2025-01-28 12:51:04 +1100

    Added initial ROS2 working packages with a dummy script executable

---
+++ src/dummyScript.cpp
@@ -0,0 +1,20 @@
+#include <gtsam/geometry/Rot3.h>
+#include <gtsam/geometry/Pose3.h>
+#include <gtsam/slam/PriorFactor.h>
+#include <gtsam/slam/BetweenFactor.h>
+#include <gtsam/navigation/GPSFactor.h>
+#include <gtsam/navigation/ImuFactor.h>
+#include <gtsam/navigation/CombinedImuFactor.h>
+#include <gtsam/nonlinear/NonlinearFactorGraph.h>
+#include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
+#include <gtsam/nonlinear/Marginals.h>
+#include <gtsam/nonlinear/Values.h>
+#include <gtsam/inference/Symbol.h>
+// #include <rclcpp/rclcpp.hpp>
+
+#include <gtsam/nonlinear/ISAM2.h>
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
